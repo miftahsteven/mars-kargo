31cd6c7d5187652b863dca4eec4509cc53358c99
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-07-30 07:19:54 +0700

    perbaikan

--- src/components/dashboard/StatusSlaCards.tsx
+++ src/components/dashboard/StatusSlaCards.tsx
@@ -50,13 +50,13 @@ export const StatusSlaCards: React.FC<StatusSlaCardsProps> = ({ summaryData: pro
     },
     {
       label: 'Dalam Transit',
-      count: "21,226", //activeData.dalam_transit,
+      count: 21226, //activeData.dalam_transit,
       pct: activeData.persentase_dalam_transit,
       color: '#ff9783',
     },
     {
       label: 'Selesai / Delivered',
-      count: "3,383",//activeData.selesai,
+      count: 3383, //activeData.selesai,
       pct: activeData.persentase_selesai,
       color: '#ec3013',
     },
